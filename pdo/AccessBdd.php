e4afef5d9ba5e86a756c3a631d84f47204dd8628
Crinon Kris <Kriscrinon@gmail.com>
2018-05-28 13:46:11 +0200

    Changement address BDD

--- pdo/AccessBdd.php
+++ pdo/AccessBdd.php
@@ -2,7 +2,7 @@
 
 
 function openDb(){
-    $connexion = mysqli_connect("mysql-assistance.alwaysdata.net", "156610_ppe","azerty","assistance_situation");
+    $connexion = mysqli_connect("localhost", "root","AzertY!59000","assistance_situation");
     mysqli_set_charset($connexion, "utf8");
     if (mysqli_connect_errno()){
         echo "Connection failed : ".mysqli_connect_error();
